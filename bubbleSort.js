b09d153b443c31e7db924906e4fa88e82cba9569
ungHeung <t01047377477@gmail.com>
2023-05-30 10:37:41 +0900

    ref : 정렬된 배열을 더이상 탐색하지 않도록 개선

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,16 +1,29 @@
-const array = [6, 5, 7, 8, 3, 4, 2, 1, 9, 0];
+const array1 = [5, 3, 2, 4, 1];
+const array2 = [1, 2, 3, 4, 5];
+const array3 = [2, 3, 4, 5, 1];
+const array4 = [2, 1, 3, 4, 5];
 
 function bubbleSort(array) {
     const arr = array;
+    const lastIdx = arr.length - 1;
+    let isSorted;
 
-    for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr.length - 1 - i; j++) {
+    for (let i = 0; i < lastIdx; i++) {
+        isSorted = true;
+        for (let j = 0; j < lastIdx - i; j++) {
             if (arr[j] > arr[j + 1]) {
+                isSorted = false;
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
         }
-        console.log(arr);
+
+        if (isSorted) {
+            break;
+        }
     }
 }
 
-bubbleSort(array);
+bubbleSort(array1);
+bubbleSort(array2);
+bubbleSort(array3);
+bubbleSort(array4);
